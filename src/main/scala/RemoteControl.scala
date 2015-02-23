08dd6b9f5a83e3d081d382abbded9c2a7150de54
romil93 <romil93@outlook.com>
2015-02-23 20:13:57 +0530

    All Unit tests passed. Logic check left

--- src/main/scala/RemoteControl.scala
+++ src/main/scala/RemoteControl.scala
@@ -2,10 +2,79 @@
  * Created by indix on 23/2/15.
  */
 
-class RemoteControl {
-  def keyPresses(minChannel: Int, maxChannel: Int, numberOfBlockedChannels: Int, blockedChannels: List[Int], numberOfChannelsToView: Int, channelsToView: List[Int]): Int = {
-    def upKeyMoves(fromChannel: Int, toChannel: Int): Int = {
-      
+class RemoteControl(minChannel: Int, maxChannel: Int, blockedChannels: List[Int], channelsToView: List[Int]) {
+
+  def upKeyMoves(fromChannelInUp: Int, toChannelInUp: Int, count: Int = 0): Int = {
+    if (fromChannelInUp == toChannelInUp) {
+      count
+    } else if (fromChannelInUp == maxChannel) {
+      upKeyMoves(minChannel, toChannelInUp, count + 1)
+    } else {
+      if (blockedChannels.contains(fromChannelInUp)){
+        upKeyMoves(fromChannelInUp + 1, toChannelInUp, count)
+      }else{
+        upKeyMoves(fromChannelInUp + 1, toChannelInUp, count + 1)
+      }
     }
   }
+
+  def downKeyMoves(fromChannelInDown: Int, toChannelInDown: Int, count: Int = 0): Int = {
+    if (fromChannelInDown == toChannelInDown) {
+      count
+    }else if (fromChannelInDown == minChannel) {
+      downKeyMoves(maxChannel, toChannelInDown, count + 1)
+    }else{
+      if (blockedChannels.contains(fromChannelInDown)){
+        downKeyMoves(fromChannelInDown - 1, toChannelInDown, count)
+
+      }else{
+        downKeyMoves(fromChannelInDown - 1, toChannelInDown, count + 1)
+      }
+    }
+  }
+
+  def numberKeyMoves(toChannelInNumberKey: Int, count: Int = 1): Int = {
+    if (toChannelInNumberKey/10 < 1 && toChannelInNumberKey%10 == toChannelInNumberKey){
+      count
+    }else{
+      numberKeyMoves(toChannelInNumberKey/10, count + 1)
+    }
+  }
+
+  def backButton(previousChannelInBack: Any, toChannelInBack: Int): Int = {
+    if (previousChannelInBack != Nil && previousChannelInBack == toChannelInBack){
+      1
+    }else{
+      10000
+    }
+  }
+
+  def movesCalculator(previousChannelInMoves: Any, fromChannel: Int, toChannel: Int): Int = {
+    val upMoves = upKeyMoves(fromChannel, toChannel)
+    val downMoves = downKeyMoves(fromChannel, toChannel)
+    val numberKeyMove = numberKeyMoves(toChannel)
+    val backMove = backButton(previousChannelInMoves, toChannel)
+
+    if (upMoves < downMoves && upMoves < numberKeyMove && upMoves < backMove) upMoves
+    else if (downMoves < upMoves && downMoves < backMove && downMoves < numberKeyMove) downMoves
+    else if (numberKeyMove < downMoves && numberKeyMove < upMoves && numberKeyMove < backMove) numberKeyMove
+    else backMove
+  }
+
+  def keyPresses(channelsToView: List[Int], previousChannel: Any = Nil, mainCount: Int = 0): Int = {
+    channelsToView match {
+      case Nil                  => throw new IllegalArgumentException("Invalid Input")
+      case x :: Nil             => throw new IllegalArgumentException("Invalid input")
+      case from :: to :: Nil    => {
+        println(from, to, mainCount)
+        val count = movesCalculator(previousChannel, from, to)
+        mainCount + count
+      }
+      case from :: to :: rest => {
+        val count = movesCalculator(previousChannel, from, to)
+        keyPresses(rest, from, mainCount + count)
+      }
+    }
+  }
+
 }
--- src/test/scala/RemoteControlTest.scala
+++ src/test/scala/RemoteControlTest.scala
@@ -1,17 +1,37 @@
 import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
 
 /**
  * Created by indix on 23/2/15.
  */
 class RemoteControlTest extends FlatSpec {
-  "RemoteControl" should "work properly for one key up" in {
-    val minChannel = 1
-    val maxChannel = 20
-    val numberOfBlockedChannels = 2
-    val blockedChannels = List(18, 19)
-    val numberOfChannelsToView = 5
-    val channelsToView = List(15,14,17,1,7)
+  "RemoteControl" should "check for upKey fucntion to work properly" in {
+    val remote = new RemoteControl(1, 25, List(), List(5,10,15,20,25))
+    val output = remote.upKeyMoves(5, 10)
+    output should be(5)
+  }
 
+  it should "check for down function to work properly" in {
+    val remote = new RemoteControl(1, 25, List(), List(5,10,15,20,25))
+    val output = remote.downKeyMoves(10, 5)
+    output should be(5)
+  }
 
+  it should "check for keyButton function to work properly" in {
+    val remote = new RemoteControl(1, 25, List(), List(5,10,15,20,25))
+    val output1 = remote.numberKeyMoves(1)
+    output1 should be(1)
+
+    val output2 = remote.numberKeyMoves(100)
+    output2 should be(3)
+  }
+
+  it should "check for backButton function to work properly" in {
+    val remote = new RemoteControl(1, 25, List(), List(5,10,15,20,25))
+    val output1 = remote.backButton(5, 5)
+    output1 should be(1)
+
+    val output2 = remote.backButton(5, 6)
+    output2 should be(10000)
   }
 }
